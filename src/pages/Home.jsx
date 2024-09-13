44ec1e8cfed9efab7d92f898c6898e0627357db1
Fabio Silveira <fabio.eds@hotmail.com>
2024-09-12 22:40:31 -0700

    AddedCode

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,17 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
+import { useState } from 'react';
 
 
 function Home() {
+    
+    const [inputValue, setInputValue] = useState("");
+    
+    const handleInput = (event) => {
+        const value = event.target.value;
+        setInputValue(value); 
+      };
     return (
         <>
             <div>
@@ -13,22 +21,32 @@ function Home() {
                     <Card>
                         <h1 className='h1-home'>TO DO LIST</h1>
                         <Form.Label className='register-task-home' htmlFor="basic-url">REGISTER TASK:</Form.Label>
-                        <InputGroup size="lg">
+                        <InputGroup className='input-size' size="lg">
                             <InputGroup.Text id="inputGroup-sizing-lg">TASK</InputGroup.Text>
                             <Form.Control
                                 aria-label="Large"
+                                type="text"
+                                value={inputValue}
+                                onChange={handleInput}
+                                
 
                             />
                         </InputGroup>
                         <Button className='btn-home' variant="secondary" size="lg">
                             REGISTER
                         </Button>
-                        
+
+                        <p>TASK-1: {inputValue}</p>
+
                         <Card className="text-center footer-home">
                             <Card.Header></Card.Header>
                             <Card.Body>
-                                <Card.Title>Special title treatment</Card.Title>
-
+                                <button class="material-symbols-outlined">
+                                    home
+                                </button>
+                                <button class="material-symbols-outlined">
+                                    add
+                                </button>
 
                             </Card.Body>
                             <Card.Footer className="text-muted">2 days ago</Card.Footer>
